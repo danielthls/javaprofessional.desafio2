f0c010c67a8edd10fc9d2569118b4b9ddf0740dd
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-01-02 07:34:08 -0300

    Criado projeto

---
+++ src/main/java/com/devsuperior/desafio2/entities/Participante.java
@@ -0,0 +1,60 @@
+package com.devsuperior.desafio2.entities;
+
+import jakarta.persistence.Entity;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.util.Objects;
+@Entity
+
+public class Participante {
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Participante(){}
+
+    public Participante(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Participante that = (Participante) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
